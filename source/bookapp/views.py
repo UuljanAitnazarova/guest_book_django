3ca020cb77f13c074f55bdbb3a5e777c618384da
uuljan <u.aitnazarova@gmail.com>
2021-03-06 15:07:59 +0600

    Created entry_delete_view, configured related urls and created templte for confirmation

--- source/bookapp/views.py
+++ source/bookapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Entry
 from .forms import EntryForm
@@ -23,3 +23,33 @@ def entry_create_view(request):
             )
             return redirect('index')
         return render(request, 'entry_create.html', context={'form':form})
+
+
+def entry_update_view(request, pk):
+    entry = get_object_or_404(Entry, id=pk)
+    if request.method == 'GET':
+        form = EntryForm(initial={
+            'author' : entry.author,
+            'email' : entry.email,
+            'text' : entry.text,
+        })
+        return render(request, 'entry_update.html', context={'form':form, 'entry':entry})
+    elif request.method == 'POST':
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            entry.author = form.cleaned_data.get('author')
+            entry.email = form.cleaned_data.get('email')
+            entry.text = form.cleaned_data.get('text')
+            entry.save()
+            return redirect('index')
+        return render(request, 'entry_update', context={'form':form, 'entry':entry})
+
+
+
+def entry_delete_view(request, pk):
+    entry = get_object_or_404(Entry, id=pk)
+    if request.method == 'GET':
+        return render(request, 'entry_delete.html', context={'entry':entry})
+    elif request.method == 'POST':
+        entry.delete()
+        return redirect('index')
