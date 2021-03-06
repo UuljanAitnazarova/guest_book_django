8b7e89ff101a272e2a2ab71c79ab47316469c59c
uuljan <u.aitnazarova@gmail.com>
2021-03-06 12:11:10 +0600

    Created GuestBook Model, registered in Admin panel and made migrations

--- source/bookapp/admin.py
+++ source/bookapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookapp.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'text', 'status']
+    list_filter = ['status']
+    search_fields = ['author']
+    fields = ['author', 'email', 'text', 'status', 'created_at', 'updated_at']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- source/bookapp/models.py
+++ source/bookapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class GuestBook(models.Model):
+
+    STATUS_CHOICE = [
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано')
+    ]
+
+    author = models.CharField(max_length=120, blank=False, null=False, verbose_name='Имя автора')
+    email = models.EmailField(blank=False, null=False, verbose_name='Почта автора')
+    text = models.TextField(max_length=1000, blank=False, null=False, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, null=False, blank=False, default=STATUS_CHOICE[0], verbose_name='Статус')
